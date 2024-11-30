b27be6db6baba89034d9b8d6fb9231b7705482c4
@Ndanga <ndangahertier@gmail.com>
2024-11-30 17:14:57 +0200

    font

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -10,49 +10,49 @@ export const Skills = () => {
         <div className="text pad-4 small-text ps-1">
           <div className="pt-3 d-dd-flex">
             <div className='pad-1 dd-flex'>
-              <div className="span-w medium-text">BOOTSTRAP</div>
+              <div className="span-w fw-6">BOOTSTRAP</div>
               <div className='spans'>
                 <span className="span-0"></span>
                 <span className='small-text ps-1'>100%</span>
               </div>
             </div>
             <div className='pad-1 dd-flex'>
-              <div className="span-w medium-text">HTML</div>
+              <div className="span-w fw-6">HTML</div>
               <div className='spans'>
                 <span className="span-1"></span>
                 <span className='small-text ps-1'>90%</span>
               </div>
             </div>
             <div className='pad-1 dd-flex'>
-              <div className="span-w medium-text">CSS</div>
+              <div className="span-w fw-6">CSS</div>
               <div className='spans'>
                 <span className="span-1"></span>
                 <span className='small-text ps-1'>90%</span>
               </div>
             </div>
             <div className='pad-1 dd-flex'>
-              <div className="span-w medium-text">PHP</div>
+              <div className="span-w fw-6">PHP</div>
               <div className='spans'>
                 <span className="span-2"></span>
                 <span className='small-text ps-1'>85%</span>
               </div>
             </div>            
             <div className='pad-1 dd-flex'>
-              <div className="span-w medium-text">JS</div>
+              <div className="span-w fw-6">JS</div>
               <div className='spans'>
                 <span className="span-3"></span>
                 <span className='small-text ps-1'>70%</span>
               </div>
             </div>
             <div className='pad-1 dd-flex'>
-              <div className="span-w medium-text">LALAVEL</div>
+              <div className="span-w fw-6">LALAVEL</div>
               <div className='spans'>
                 <span className="span-4"></span>
                 <span className='small-text ps-1'>50%</span>
               </div>
             </div>
             <div className='pad-1 dd-flex'>
-              <div className="span-w medium-text">REACT JS</div>
+              <div className="span-w fw-6">REACT JS</div>
               <div className='spans'>
                 <span className="span-4"></span>
                 <span className='small-text ps-1'>50%</span>
