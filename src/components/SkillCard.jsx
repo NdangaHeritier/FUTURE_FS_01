0f2fa736930caa7ce447de096f39940043c1c7ab
@Ndanga <163980624+NdangaHeritier@users.noreply.github.com>
2025-04-23 18:46:09 +0200

    Update SkillCard.jsx

--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SkillCard = ({name}) => {
     return (
-        <div className='p-1'>
+        <div className=''>
             <span className="bg-white/5 border border-white/20 text-slate-200 rounded-full px-3 py-1 text-xs">
                 {name}
             </span>
